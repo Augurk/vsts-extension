a460ae4b618199e6ad4a557879e294e8c0b0fc38
Jonathan Mezach <jonathanm@infosupport.com>
2018-12-17 21:45:25 +0100

    Add grouping of features before publishing

--- src/build-task/AugurkCLI/publishCommand.ts
+++ src/build-task/AugurkCLI/publishCommand.ts
@@ -1,12 +1,24 @@
 import tl = require('azure-pipelines-task-lib/task');
 import trm = require('azure-pipelines-task-lib/toolrunner');
+import path = require('path');
+import { stringify } from 'querystring';
 
 export async function publishCommand(tool: trm.ToolRunner) {
-    // Build and run the publish command
+    // Find the feature files we're going to publish
     const features = tl.getPathInput('features', true);
     const featureFiles = tl.findMatch(process.cwd(), features);
 
-    tl.debug(featureFiles.join(','));
+    // Group the found files by their parent directory
+    const groupedFeatures = featureFiles.reduce<{ [index: string]: string[] }>((groups, item) => {
+        const parent = path.dirname(item).split(path.sep).pop() as string;
+        groups[parent] = groups[parent] || [];
+        groups[parent].push(item);
+        return groups;
+    }, {});
+
+    for (const key of Object.keys(groupedFeatures)) {
+        tl.debug('Found group ' + key);
+    }
 
     const publishCommand = tool;
     const publishResult = await publishCommand.exec();
